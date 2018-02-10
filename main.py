c368825ff0ecdd5e6ee2a2309c750b63900a980e
huiyineo <huiyi2109@gmail.com>
2018-02-10 15:44:03 +0800

    updated output

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ app = Flask(__name__, static_url_path='/static')
 def hello_world():
   return 'Hello, World!'
 
-@app.route('/test/')
+@app.route('/shop/')
 # @app.route('/hello/<name>')
-def new():
-    return render_template('checkout.html')
+def shop():
+    return render_template('shop.html')
 
 
 @app.route('/')
 # @app.route('/hello/<name>')
 def hello():
     return render_template('index.html')
+
+@app.route('/space123/')
+# @app.route('/hello/<name>')
+def new():
+    return render_template('single.html')
 
 if __name__ == '__main__':
   app.run()
