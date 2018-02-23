7ab9c51593d1184c2b3e47313af5a2564abcbe65
huiyineo <huiyi2109@gmail.com>
2018-02-24 02:31:03 +0800

    new ruumi with many hardcoded pages

--- main.py
+++ main.py
@@ -3,25 +3,38 @@ from flask import render_template
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/hello/')
-def hello_world():
-  return 'Hello, World!'
-
 @app.route('/shop/')
 # @app.route('/hello/<name>')
 def shop():
     return render_template('shop.html')
 
-
 @app.route('/')
-# @app.route('/hello/<name>')
 def hello():
     return render_template('index.html')
 
+@app.route('/guest/')
+def guest_index():
+    return render_template('guest_index.html')
+
 @app.route('/space123/')
-# @app.route('/hello/<name>')
-def new():
+def space123():
     return render_template('single.html')
+
+@app.route('/checkout/', methods=['GET', 'POST'])
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/done/', methods=['GET', 'POST'])
+def done():
+    return render_template('done.html')
+
+@app.route('/host/')
+def host_index():
+    return render_template('host_index.html')
+
+@app.route('/myacc/')
+def account():
+    return render_template('myacc.html')
 
 if __name__ == '__main__':
   app.run()
